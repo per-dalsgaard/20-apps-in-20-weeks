b8a3259f6912e570f2f37de67c8a42332f5975bb
Per Dalsgaard Kristensen <per@codeph.dk>
2017-03-27 22:22:40 +0200

    Add UIPickerView for states

--- App 01 - Miracle Pills/Miracle Pills/ViewController.swift
+++ App 01 - Miracle Pills/Miracle Pills/ViewController.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource {
 
     @IBOutlet weak var successImageView: UIImageView!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var chooseStateButton: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York",]
     
     @IBAction func buyNowButtonPressed() {
         for view in view.subviews {
@@ -18,6 +26,33 @@ class ViewController: UIViewController {
         }
         
         successImageView.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    @IBAction func chooseStateButtonPressed() {
+        let selectStateMode = !statePicker.isHidden
+        if selectStateMode {
+            let selectedState = states[statePicker.selectedRow(inComponent: 0)]
+            chooseStateButton.setTitle(selectedState, for: .normal)
+        }
+        
+        countryLabel.isHidden = !selectStateMode
+        countryTextField.isHidden = !selectStateMode
+        zipCodeLabel.isHidden = !selectStateMode
+        zipCodeTextField.isHidden = !selectStateMode
+        
+        statePicker.isHidden = selectStateMode
     }
 }
 
