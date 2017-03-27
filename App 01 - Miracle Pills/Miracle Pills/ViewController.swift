26bc3691284d03061815e28d163fb94124566b0e
Per Dalsgaard Kristensen <per@codeph.dk>
2017-03-27 17:48:51 +0200

    Complete app 01

--- App 01 - Miracle Pills/Miracle Pills/ViewController.swift
+++ App 01 - Miracle Pills/Miracle Pills/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var successImageView: UIImageView!
+    
+    @IBAction func buyNowButtonPressed() {
+        for view in view.subviews {
+            view.isHidden = true
+        }
+        
+        successImageView.isHidden = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
