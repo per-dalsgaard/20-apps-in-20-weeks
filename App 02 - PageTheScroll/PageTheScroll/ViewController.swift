f36707591af747a1d0a640d348474cede37f8217
Per Dalsgaard Kristensen <per@codeph.dk>
2017-03-28 22:56:54 +0200

    App 02 - Add scrollView and GestureRecognizer

--- App 02 - PageTheScroll/PageTheScroll/ViewController.swift
+++ App 02 - PageTheScroll/PageTheScroll/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollViewWidth = scrollView.frame.size.width
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollViewWidth / 2 + scrollViewWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            
+        }
+        
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
-
+    
+   
+    @IBAction func leftSwipe(_ sender: UISwipeGestureRecognizer) {
+        // TODO
+    }
+    
+    @IBAction func rightSwipe(_ sender: UISwipeGestureRecognizer) {
+        // TODO
+        scrollView.setContentOffset(CGPoint(x: 10, y: 0), animated: true)
+    }
 }
 
