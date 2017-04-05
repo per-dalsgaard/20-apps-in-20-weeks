9b7b860a3bfa3e972c2fef4ca876a72255c77aec
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-05 22:36:29 +0200

    App 06 - Initial commit

---
+++ App 06 - PartyRockApp/PartyRockApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Per Kristensen on 05/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
