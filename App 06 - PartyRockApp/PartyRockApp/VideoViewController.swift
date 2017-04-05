0968cb552d476cd35259237a453e1134e002e269
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-05 23:35:24 +0200

    App 06 - Assets, fonts and table view cell

---
+++ App 06 - PartyRockApp/PartyRockApp/VideoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoViewController.swift
+//  PartyRockApp
+//
+//  Created by Per Kristensen on 05/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
