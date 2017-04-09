4bc8b0bf920e19a93ebf5368bae1d84ec0d6729d
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-09 23:16:26 +0200

    Setup Core Data and add test data

---
+++ App 08 - DreamLister/DreamLister/ItemTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemTableViewCell.swift
+//  DreamLister
+//
+//  Created by Per Kristensen on 08/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    
+    func configureCell(item: Item) {
+        titleLabel.text = item.title
+        priceLabel.text = "$\(item.price)"
+        detailsLabel.text = item.details
+    }
+    
+}
---
+++ App 08 - DreamLister/DreamLister/MainViewController.swift
@@ -0,0 +1,139 @@
+//
+//  MainViewController.swift
+//  DreamLister
+//
+//  Created by Per Kristensen on 08/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    var controller: NSFetchedResultsController<Item>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+//        generateTestData()
+        attemptFetch()
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemTableViewCell", for: indexPath) as! ItemTableViewCell
+        configureCell(cell: cell, indexPath: indexPath)
+        return UITableViewCell()
+    }
+    
+    func configureCell(cell: ItemTableViewCell, indexPath: IndexPath) {
+        // TODO: Update cell
+        let item = controller.object(at: indexPath)
+        cell.configureCell(item: item)
+    }
+    
+    func attemptFetch() {
+        let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        let dateSort = NSSortDescriptor(key: "created", ascending: false)
+        fetchRequest.sortDescriptors = [dateSort]
+        
+        controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try controller.performFetch()
+        } catch {
+            let error = error as NSError
+            print("\(error)")
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        case .update:
+            if let indexPath = indexPath {
+                let cell = tableView.cellForRow(at: indexPath) as! ItemTableViewCell
+                configureCell(cell: cell, indexPath: indexPath)
+            }
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
+    
+    func generateTestData() {
+        let item = Item(context: context)
+        item.title = "Test 1"
+        item.price = 120
+        item.details = "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum."
+        
+        let item2 = Item(context: context)
+        item2.title = "Test 1"
+        item2.price = 120
+        item2.details = "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum."
+        
+        let item3 = Item(context: context)
+        item3.title = "Test 1"
+        item3.price = 120
+        item3.details = "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum."
+        
+        let item4 = Item(context: context)
+        item4.title = "Test 1"
+        item4.price = 120
+        item4.details = "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum."
+        
+        let item5 = Item(context: context)
+        item5.title = "Test 1"
+        item5.price = 120
+        item5.details = "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum."
+        
+        ad.saveContext()
+    }
+
+}
+
