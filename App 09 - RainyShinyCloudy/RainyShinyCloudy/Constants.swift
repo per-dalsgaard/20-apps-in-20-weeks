006f0556d7f86abac544b7019fcc19569fbbfd29
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-13 23:27:23 +0200

    Add constant file for API information

---
+++ App 09 - RainyShinyCloudy/RainyShinyCloudy/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  RainyShinyCloudy
+//
+//  Created by Per Kristensen on 13/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import Foundation
---
+++ App 09 - RainyShinyCloudy/RainyShinyCloudy/WeatherViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherViewController.swift
+//  RainyShinyCloudy
+//
+//  Created by Per Kristensen on 13/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImageView: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        return cell
+    }
+
+}
+
