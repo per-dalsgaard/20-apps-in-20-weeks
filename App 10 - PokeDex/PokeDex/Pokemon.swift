1c92bc99e71bf42b28799a97b12bfab7be1b003a
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-15 10:43:33 +0200

    Add Pokemon model and collection view

---
+++ App 10 - PokeDex/PokeDex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Per Kristensen on 15/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+    
+}
