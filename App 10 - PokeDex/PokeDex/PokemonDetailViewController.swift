cc68977a3e33c413743a6048fb085ce36f0fdcfd
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-16 22:39:15 +0200

    Configure stack views on Details view controller and update Pokemon model

--- App 10 - PokeDex/PokeDex/PokemonDetailViewController.swift
+++ App 10 - PokeDex/PokeDex/PokemonDetailViewController.swift
@@ -12,9 +12,25 @@ class PokemonDetailViewController: UIViewController {
     
     var pokemon: Pokemon!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexIdLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var baseAttackLabel: UILabel!
+    @IBOutlet weak var currentEvolutionImageView: UIImageView!
+    @IBOutlet weak var nextEvolutionImageView: UIImageView!
+    @IBOutlet weak var evolutionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 }
