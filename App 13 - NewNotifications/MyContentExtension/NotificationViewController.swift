65ba4089c5d64e9db073e0a290844fc5529992b8
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-19 23:04:41 +0200

    Configure and schedule local notification

---
+++ App 13 - NewNotifications/MyContentExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  MyContentExtension
+//
+//  Created by Per Kristensen on 19/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
--- App 13 - NewNotifications/NewNotifications/ViewController.swift
+++ App 13 - NewNotifications/NewNotifications/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Notification access granted!")
+            } else {
+                print(error?.localizedDescription ?? "TODO")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func notifyButtonPressed(_ sender: UIButton) {
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Erro scheduling notification")
+            }
+        })
     }
-
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        guard let imageUrl = Bundle.main.url(forResource: "rick_grimes", withExtension: "gif") else {
+            completion(false)
+            return
+        }
+        
+        var attachment: UNNotificationAttachment
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageUrl, options: .none)
+        
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New notifications"
+        notif.subtitle = "These are great!"
+        notif.body = "The new notification options in iOS 10 are what I've always dreamed of!"
+        notif.attachments = [attachment]
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error.debugDescription)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }
 }
 
