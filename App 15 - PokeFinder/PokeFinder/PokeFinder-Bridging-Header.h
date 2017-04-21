c08c460d542d1b66cde0a0a730c38cdca38aa276
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-21 23:23:11 +0200

    Add assets and configure MapView

---
+++ App 15 - PokeFinder/PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Per Kristensen on 21/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+#import "GeoFire.h"
