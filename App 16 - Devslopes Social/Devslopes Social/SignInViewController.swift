b75dd7dc7ba20e0989239b77606e35c095338ca5
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-24 22:15:54 +0200

    Basic sign in view layout

---
+++ App 16 - Devslopes Social/Devslopes Social/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  Devslopes Social
+//
+//  Created by Per Kristensen on 24/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
