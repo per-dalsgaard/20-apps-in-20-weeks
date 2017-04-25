94f88f5bbcd7782ed9d9a8e32bbfe998916009cc
Per Dalsgaard Kristensen <per@codeph.dk>
2017-04-25 22:47:52 +0200

    Add email password auth

--- App 16 - Devslopes Social/Devslopes Social/SignInViewController.swift
+++ App 16 - Devslopes Social/Devslopes Social/SignInViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: CustomTextField!
+    @IBOutlet weak var passwordTextField: CustomTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: RoundCornersButton) {
+        if let email = emailTextField.text , let password = passwordTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("PDK: Email user authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("PDK: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("PDK: New email user created")
+                        }
+                    })
+                }
+            })
+        }
     }
 }
