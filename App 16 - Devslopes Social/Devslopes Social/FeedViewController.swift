ac4cb5a6dbb91e813de06e17ceac4424bad4ded5
Per Kristensen <perkristensen@pers-air.home>
2017-04-25 23:26:32 +0200

    Add keychain wrapper and use it after successful login

---
+++ App 16 - Devslopes Social/Devslopes Social/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  Devslopes Social
+//
+//  Created by Per Kristensen on 25/04/2017.
+//  Copyright © 2017 Per Dalsgaard. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- App 16 - Devslopes Social/Devslopes Social/SignInViewController.swift
+++ App 16 - Devslopes Social/Devslopes Social/SignInViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import SwiftKeychainWrapper
 
 class SignInViewController: UIViewController {
 
@@ -16,24 +17,45 @@ class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let _ = KeychainWrapper.standard.string(forKey: KEY_UID) {
+            performSegue(withIdentifier: FEED_SEGUE_IDENTIFIER, sender: nil)
+        }
+    }
+    
+    // MARK: - IBActions
     
     @IBAction func signInButtonPressed(_ sender: RoundCornersButton) {
         if let email = emailTextField.text , let password = passwordTextField.text {
             FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
                 if error == nil {
                     print("PDK: Email user authenticated with Firebase")
+                    if let user = user {
+                        self.completeSignIn(id: user.uid)
+                    }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
                         if error != nil {
                             print("PDK: Unable to authenticate with Firebase using email")
                         } else {
                             print("PDK: New email user created")
+                            if let user = user {
+                                self.completeSignIn(id: user.uid)
+                            }
                         }
                     })
                 }
             })
         }
+    }
+    
+    func completeSignIn(id: String) {
+        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
+        print("PDK: Keychain result: \(keychainResult)")
+        performSegue(withIdentifier: FEED_SEGUE_IDENTIFIER, sender: nil)
     }
 }
