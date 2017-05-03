a70b0b3fed1b1095fde66c90f3f56789989d7c60
per-dalsgaard <per@codeph.dk>
2017-05-03 22:45:08 +0200

    Handle Firebase errors

--- App 17 - DevChat/DevChat/AuthService.swift
+++ App 17 - DevChat/DevChat/AuthService.swift
@@ -9,6 +9,8 @@
 import Foundation
 import Firebase
 
+typealias Completion = (_ errorMsg: String?, _ data: AnyObject?) -> Void
+
 class AuthService {
     
     private static let _instance = AuthService()
@@ -17,27 +19,27 @@ class AuthService {
         return _instance
     }
     
-    func login(email: String, password: String) {
+    func login(email: String, password: String, onComplete: Completion?) {
         FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
             
             if error != nil {
                 
-                if let errorCode = FIRAuthErrorCode(rawValue: (error! as NSError).code) {
+                if let errorCode = FIRAuthErrorCode(rawValue: error!._code) {
                     
                     if errorCode == .errorCodeUserNotFound {
                         
                         FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
                             
                             if error != nil {
-                                // TODO: Show error to user
+                                self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
                             } else {
                                 
                                 if user?.uid != nil {
                                     FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
                                         if error != nil {
-                                            // TODO: Show error to user
+                                            self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
                                         } else {
-                                            // Successfully logged in
+                                            onComplete?(nil, user)
                                         }
                                     })
                                 }
@@ -45,11 +47,31 @@ class AuthService {
                         })
                     } else {
                         // TODO: Handle all other errors
+                        self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
                     }
                 }
             } else {
                 // Successfully logged in
+                onComplete?(nil, user)
             }
         })
+    }
+    
+    func handleFirebaseError(error: NSError, onComplete: Completion?) {
+        print(error.debugDescription)
+        if let errorCode = FIRAuthErrorCode(rawValue: error.code) {
+            switch errorCode {
+            case .errorCodeInvalidEmail:
+                onComplete?("Invalid email address", nil)
+            case .errorCodeWeakPassword:
+                onComplete?("Password should be at least 6 characteres")
+            case .errorCodeWrongPassword:
+                onComplete?("Invalid password", nil)
+            case .errorCodeEmailAlreadyInUse, .errorCodeAccountExistsWithDifferentCredential:
+                onComplete?("Could not create account. Email already in use", nil)
+            default:
+                onComplete?("There was a problem authenticating. Try again.", nil)
+            }
+        }
     }
 }
--- App 17 - DevChat/DevChat/LogInViewController.swift
+++ App 17 - DevChat/DevChat/LogInViewController.swift
@@ -22,7 +22,16 @@ class LogInViewController: UIViewController {
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         if let email = emailTextField.text , let password = passwordTextField.text , (email.characters.count > 0 && password.characters.count > 0) {
-                AuthService.instance.login(email: email, password: password)
+                AuthService.instance.login(email: email, password: password, onComplete: { (errorMsg, data) in
+                    if let errorMsg = errorMsg {
+                        let alert = UIAlertController(title: "Error Authentication", message: errorMsg, preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                        return
+                    }
+                    
+                    self.dismiss(animated: true, completion: nil)
+                })
             } else {
             let alert = UIAlertController(title: "Username and Password required", message: "You must enter both an email and a password", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
